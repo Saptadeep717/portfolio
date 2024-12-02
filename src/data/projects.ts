bb1bfdaf1745a5a11eb350dd4e09ed578708362d
Saptadeep <saptadeepsenab717@gmail.com>
2024-12-02 15:14:41 +0530

    projects changed 2.0

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,7 +5,8 @@ export const projects: Project[] = [
     id: 1,
     title: "egTeam an ed-tech platform",
     description:
-      "An easy-to-use platform that provided quick access to instructional materials and interactive courses built with Node.js and Express.js, handling authentication,authorization and payment.",
+      "An easy-to-use platform that provided quick access to instructional materials and interactive"+ 
+      "courses built with Node.js,Express.js, handling authentication,authorization and payment.",
     technologies: [
       "Node.js",
       "Express.js",
@@ -21,7 +22,8 @@ export const projects: Project[] = [
     id: 2,
     title: "egDocs a real time text editor",
     description:
-      "A real-time document collaboration platform with editing tools,allowing multiple users to edit concurrently and view changes in real time",
+      "A real-time document collaboration platform with editing tools,allowing multiple users to"+ 
+      "edit concurrently and view changes in real time",
     technologies: [
       "Node.js",
       "Express.js",
@@ -37,7 +39,8 @@ export const projects: Project[] = [
     id: 3,
     title: "C++ Application",
     description:
-      "An user-interactive website that includes several C++ applications and tiny projects based on real world DSA",
+      "An user-interactive website that includes several C++ applications and tiny projects"+ 
+      "based on real world DSA solving",
     technologies: ["C++", "HTML", "CSS", "Javascript"],
     githubUrl: "https://github.com/Saptadeep717/cpp",
     liveUrl: "https://cpp-projects.pages.dev/",
